d0610bfd803cd4334d3f93d157b6185d78e70264
Adam Shumann <ashumann13@gmail.com>
2021-01-14 15:39:31 -0600

    Updates to vk stuff

--- screepsplus/loan/vk/gen-secrets.sh
+++ screepsplus/loan/vk/gen-secrets.sh
@@ -1,4 +1,4 @@
-for S in {mmo,splus1,splus2,swc,botarena}
+for S in {mmo,season,splus1,splus2,swc,botarena}
 do
   kubectl -n loan delete secret/vk-config-$S
   kubectl -n loan create secret generic vk-config-$S \
