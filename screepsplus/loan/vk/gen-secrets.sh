ab38d0d141d263235d831dc86752cc458ec5f8de
Adam Shumann <ashumann13@gmail.com>
2021-06-18 10:46:59 -0500

    Correct postgres mount point

--- screepsplus/loan/vk/gen-secrets.sh
+++ screepsplus/loan/vk/gen-secrets.sh
@@ -1,7 +1,8 @@
 for S in {mmo,season,splus1,splus2,swc,botarena}
 do
-  kubectl -n loan delete secret/vk-config-$S
+  # kubectl -n loan delete secret/vk-config-$S
   kubectl -n loan create secret generic vk-config-$S \
     --from-file=$S/.settings.json \
-    --from-file=$S/DB_LOCATION
+    --from-file=$S/DB_LOCATION \
+    --dry-run=client -o yaml | kubectl -n loan apply -f -
 done
